83e73ca2cb4a737d9cc0c576ca62dd12d143f784
Ilya Tazalov <tazalov9@gmail.com>
2026-02-28 20:47:55 +0300

    chore: test pre-commit

--- src/extension.ts
+++ src/extension.ts
@@ -19,9 +19,7 @@ export function activate(context: vscode.ExtensionContext1) {
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage(
-        'Hello World from vscode-colored-tags!',
-      )
+      vscode.window.showInformationMessage('Hello World from colored-tags!')
     },
   )
 
