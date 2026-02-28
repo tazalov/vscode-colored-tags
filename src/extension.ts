37d018a0e9125989ce1d31d5ab70c3e44755796a
Ilya Tazalov <tazalov9@gmail.com>
2026-02-28 20:50:59 +0300

    chore: test pre-commit

--- src/extension.ts
+++ src/extension.ts
@@ -4,11 +4,11 @@ import * as vscode from 'vscode'
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext1) {
+export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(
-    'Congratulations, your extension "vscode-colored-tags" is now active!',
+    'Congratulations, your extension "vscode-colored-tags" is active!',
   )
 
   // The command has been defined in the package.json file
